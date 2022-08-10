d8464aad7b95c03727a3f159aaae146379e7e045
E134D3 <110093449+E134D3@users.noreply.github.com>
2022-08-10 17:10:27 +0300

    Бот для перехода ВИ

--- Bot.js
+++ Bot.js
@@ -11,25 +11,20 @@
 
 let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress", "Взаимодействие PHP и MySQL. Подключение к базе данных MySQL"];
 let keyword = keywords[getRandom(0, keywords.length)];
-let sb_form_q = document.getElementById("sb_form_q")
+let btn = document.getElementById("search_icon");
 let links = document.links;
 
-if(sb_form_go !== undefined) {
-  document.getElementById("sb_form_q").value = keyword;
-  sb_form_go.click();
-} else {
+if (btn !== null) {
+  document.getElementById("sb_form_q").value = "10 самых популярных шрифтов от Google";
+  btn.click();}
+else {
 
   for (let i = 0; i < links.length; i++) {
     if (links[i].href.indexOf("napli.ru") !== -1) {
       let link = links[i];
-      console.log("Нашел строку " + link[i]);
+      console.log("Нашел строку " + link);
       link.click();
-      break;
-    }
-  }
-}
+      break;}}}
 
-
-function getRandom(min, max) {
-  return Math.floor(Math.random()*(max - min) + min); 
-}
+      function getRandom(min, max){
+      return Math.floor(Math.random()*(max - min) + min);}
